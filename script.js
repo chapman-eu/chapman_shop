c4715637805f491fb9c57d3be267eefe7743175d
chapman-eu <chapman_europe@proton.me>
2026-01-12 19:27:31 +0100

    KINGSIZE

--- script.js
+++ script.js
@@ -15,29 +15,35 @@ const DELIVERY_FEE = 5.00;
 // PRODUCTS
 const PRODUCTS = [
   // CHERRY
-  { id: "cherry-compact-single",  name: "Chapman Compact Cherry",        price: 13.5, img: "img/compact_cherry.png",        inStock: true, isNew: false },
-  { id: "cherry-slims-single",    name: "Chapman Slims Cherry",          price: 13,   img: "img/slims_cherry.png",          inStock: true, isNew: true },
-  { id: "cherry-compact-x10",     name: "Chapman Compact Cherry (x10)",  price: 115,  img: "img/compact_cherry_x10.png",    inStock: false, isNew: false },
+  { id: "cherry-kingsize-single",  name: "Chapman KINGSIZE Cherry",        price: 15, img: "img/kingsize_cherry.png",        inStock: true, isNew: true },
+  { id: "cherry-compact-single",  name: "Chapman Compact Cherry",        price: 10, img: "img/compact_cherry.png",        inStock: true, isNew: false },
+  { id: "cherry-slims-single",    name: "Chapman Slims Cherry",          price: 11,5,   img: "img/slims_cherry.png",          inStock: true, isNew: true },
+  { id: "cherry-compact-x10",     name: "Chapman Compact Cherry (x10)",  price: 90,  img: "img/compact_cherry_x10.png",    inStock: false, isNew: false },
   { id: "cherry-slims-x10",       name: "Chapman Slims Cherry (x10)",    price: 110,  img: "img/slims_cherry_x10.png",      inStock: true, isNew: true },
+  { id: "cherry-kingsize-x10",       name: "Chapman KINGSIZE Cherry (x10)",    price: 125,  img: "img/kingsize_cherry_x10.png",      inStock: true, isNew: true },
 
   // BROWN
-  { id: "brown-compact-single",   name: "Chapman Compact Brown",         price: 11,   img: "img/compact_brown.png",         inStock: true, isNew: false },
-  { id: "brown-compact-x10",      name: "Chapman Compact Brown (x10)",   price: 100,  img: "img/compact_brown_x10.png",     inStock: true, isNew: false },
+  { id: "brown-kingsize-single",   name: "Chapman KINGSIZE Brown",         price: 15,   img: "img/kingsize_brown.png",         inStock: true, isNew: true },
+  { id: "brown-compact-single",   name: "Chapman Compact Brown",         price: 10,   img: "img/compact_brown.png",         inStock: true, isNew: false },
+  { id: "brown-compact-x10",      name: "Chapman Compact Brown (x10)",   price: 90,  img: "img/compact_brown_x10.png",     inStock: true, isNew: false },
+  { id: "brown-kingsize-x10",      name: "Chapman KINGSIZE Brown (x10)",   price: 125,  img: "img/kingsize_brown_x10.png",     inStock: true, isNew: true },
 
   // GRAPE
-  { id: "grape-compact-single",   name: "Chapman Compact Grape",         price: 12.5, img: "img/compact_grape.png",         inStock: true, isNew: true },
-  { id: "grape-compact-x10",      name: "Chapman Compact Grape (x10)",   price: 110,  img: "img/compact_grape_x10.png",     inStock: true, isNew: true },
+  { id: "grape-compact-single",   name: "Chapman Compact Grape",         price: 10, img: "img/compact_grape.png",         inStock: true, isNew: true },
+  { id: "grape-compact-x10",      name: "Chapman Compact Grape (x10)",   price: 90,  img: "img/compact_grape_x10.png",     inStock: true, isNew: false },
 
   // VANILLA
+  { id: "vanilla-kingsize-single",   name: "Chapman KINGSIZE Vanilla",         price: 15,   img: "img/kingsize_vanilla.png",         inStock: true, isNew: true },
   { id: "vanilla-slims-single",   name: "Chapman Slims Vanilla",         price: 13,   img: "img/slims_vanilla.png",         inStock: true, isNew: true },
   { id: "vanilla-slims-x10",      name: "Chapman Slims Vanilla (x10)",   price: 110,  img: "img/slims_vanilla_x10.png",     inStock: true, isNew: true },
+  { id: "vanilla-kingsize-x10",      name: "Chapman KINGSIZE Vanilla (x10)",   price: 125,  img: "img/kingsize_vanilla_x10.png",     inStock: true, isNew: true },
 
   // ICEBERRY
-  { id: "iceberry-slims-single",  name: "Chapman Slims IceBerry",        price: 13,   img: "img/slims_iceberry.png",        inStock: true, isNew: true },
+  { id: "iceberry-slims-single",  name: "Chapman Slims IceBerry",        price: 11,5,   img: "img/slims_iceberry.png",        inStock: true, isNew: true },
   { id: "iceberry-slims-x10",     name: "Chapman Slims IceBerry (x10)",  price: 110,  img: "img/slims_iceberry_x10.png",    inStock: true, isNew: true },
 
   // GREEN
-  { id: "green-slims-single",     name: "Chapman Slims Green",           price: 13,   img: "img/slims_green.png",           inStock: true, isNew: true },
+  { id: "green-slims-single",     name: "Chapman Slims Green",           price: 11,5,   img: "img/slims_green.png",           inStock: true, isNew: true },
   { id: "green-slims-x10",        name: "Chapman Slims Green (x10)",     price: 110,  img: "img/slims_green_x10.png",       inStock: true, isNew: true }
 ];
 
